d4244626d86b32c350ea508eccce43aad1c838af
LimeMarble <l1m3marbl3@gmail.com>
2026-08-08 01:19:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hamster-field-cloners/',
+  base: '/hamster-cloners/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
