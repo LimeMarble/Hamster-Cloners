5dd5b05cd172693d050d5a4389f10d928aa4f28d
LimeMarble <l1m3marbl3@gmail.com>
2026-08-13 08:53:42 +0300

    corn cost nerf

--- src/game/crops.js
+++ src/game/crops.js
@@ -157,7 +157,7 @@ export const CROP_PERFECTIONS = {
     id: 'mirrorCorn',
     cropId: 'corn',
     name: 'Mirror Corn',
-    cost: 8e13,
+    cost: 4e12,
     baseYield: 5,
     hamsterEfficiencyBonus: -0.5,
     diagonalTargetEffectMultiplier: 4,
